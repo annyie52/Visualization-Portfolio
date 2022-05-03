0129b06eb6847ce9a54f662b0162848c21cb0c99
annyie52 <yeaeun2599@gmail.com>
2022-05-03 19:44:06 +0900

    second setting

--- Portfolio/single_pages/views.py
+++ Portfolio/single_pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(
+        request,
+        'single_pages/home.html'
+    )
+
+def purpose(request):
+    return render(
+        request,
+        'single_pages/purpose.html'
+    )
